07b26377609d8e2c6e9b1dbb6b92852f903fa659
Gal A. Kaminka <galk@cs.biu.ac.il>
2025-12-29 10:58:50 +0200

    Reordered columns in combined, so that result preceds counts. Also fixed a typo

--- unify.sh
+++ unify.sh
@@ -2,17 +2,17 @@
 COUNT_PATH=$1
 RESULTS_PATH=$2
 
-cat $COUNT_PATH | cut -d, -f1 | sort > tmp1.csv
-cat $RESULTS_PATH | cut -d, -f1 | sort > tmp2.csv
+cat $COUNT_PATH | cut -d, -f1 | sort >tmp1.csv
+cat $RESULTS_PATH | cut -d, -f1 | sort >tmp2.csv
 if cmp tmp1.csv tmp2.csv; then
     echo ""
 else
-    echo "filenemaes differ. aborting."
+    echo "problem filenames differ. aborting."
     exit 1
 fi
 
-cat $RESULTS_PATH | cut -d, -f2- > tmp3.csv
-paste -d',' $COUNT_PATH tmp3.csv > combined.csv
+cat $COUNT_PATH | cut -d, -f2- >tmp3.csv
+paste -d',' $RESULTS_PATH tmp3.csv >combined.csv
 
 rm tmp1.csv
 rm tmp2.csv
